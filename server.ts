80780a6ae4c11d9703e5630d4d7d5dbd147bcb91
Aimee T <aimeetacchi@hotmail.co.uk>
2019-01-26 15:19:06 +0000

    amend package.json

--- server.ts
+++ server.ts
@@ -1,58 +1,49 @@
-// import 'zone.js/dist/zone-node';
-// import 'reflect-metadata';
-//import { enableProdMode } from '@angular/core';
+import 'zone.js/dist/zone-node';
+import { enableProdMode } from '@angular/core';
+
 // Express Engine
-//import { ngExpressEngine } from '@nguniversal/express-engine';
+import { ngExpressEngine } from '@nguniversal/express-engine';
 // Import module map for lazy loading
-//import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
-const express = require('express');
-//import * as bodyParser from 'body-parser';
-const path = require('path');
-//import { join } from 'path';
-
-const axios = require('axios');
-
+import * as express from 'express';
+import { join } from 'path';
+import axios from 'axios';
 // Faster server renders w/ Prod mode (dev mode never needed)
-//enableProdMode();
+enableProdMode();
 
 // Express server
 const app = express();
 
-// BODY PARSER ===
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-
-//create middleware to use throughout app
-// app.use(function (req, res, next) {
-//   //set headers to allow cross origin request.
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-
-// const PORT = process.env.PORT || 4000;
-// const DIST_FOLDER = join(process.cwd(), 'dist');
+const PORT = process.env.PORT || 4000;
+const DIST_FOLDER = join(process.cwd(), 'dist');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-// const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main.bundle');
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./server/main');
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-// app.engine('html', ngExpressEngine({
-//   bootstrap: AppServerModuleNgFactory,
-//   providers: [
-//     provideModuleMap(LAZY_MODULE_MAP)
-//   ]
-// }));
+app.engine('html', ngExpressEngine({
+  bootstrap: AppServerModuleNgFactory,
+  providers: [
+    provideModuleMap(LAZY_MODULE_MAP)
+  ]
+}));
 
-// app.set('view engine', 'html');
-// app.set('views', join(DIST_FOLDER, 'browser'));
+app.set('view engine', 'html');
+app.set('views', join(DIST_FOLDER, 'browser'));
 
-/* - Example Express Rest API endpoints -
-  app.get('/api/**', (req, res) => { });
-*/
+// Example Express Rest API endpoints
+// app.get('/api/**', (req, res) => { });
+
+// Server static files from /browser
+app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
+  maxAge: '1y'
+}));
+
+// All regular routes use the Universal engine
+app.get('*', (req, res) => {
+  res.render('index', { req });
+});
 
 
 
@@ -75,29 +66,13 @@ app.get('/api/getdata', (req, res) => {
 })
 
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/my-treehouse-portfolio'));
-
-app.get('/*', function (req, res) {
-
-  res.sendFile(path.join(__dirname + '/dist/my-treehouse-portfolio/index.html'));
+// Start up the Node server
+app.listen(PORT, () => {
+  console.log(`Node Express server listening on http://localhost:${PORT}`);
 });
 
 
-// // Server static files from /browser
-// app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
-//   maxAge: '1y'
-// }));
 
-// // ALl regular routes use the Universal engine
-// app.get('*', (req, res) => {
-//   res.render('index', { req });
-// });
 
-// Start up the Node server
-// app.listen(PORT, () => {
-//   console.log(`Node Express server listening on http://localhost:${PORT}`);
-// });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
