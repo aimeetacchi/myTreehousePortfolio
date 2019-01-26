2a62f68f280b4bca39896e4b9dba4319ad7556bd
Aimee T <aimeetacchi@hotmail.co.uk>
2019-01-26 16:56:46 +0000

    amends

--- server.ts
+++ server.ts
@@ -1,12 +1,16 @@
 import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
 import { enableProdMode } from '@angular/core';
+// Express Engine
+import { ngExpressEngine } from '@nguniversal/express-engine';
+// Import module map for lazy loading
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 import { join } from 'path';
-import { readFileSync } from 'fs';
 
-import axios from 'axios';
+const axios = require('axios');
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -14,20 +18,25 @@ enableProdMode();
 // Express server
 const app = express();
 
+// BODY PARSER ===
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//create middleware to use throughout app
+app.use(function (req, res, next) {
+  //set headers to allow cross origin request.
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
-// Our index.html we'll use as our template
-const template = readFileSync(join(DIST_FOLDER, 'browser', 'index.html')).toString();
-
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main.bundle');
-
-
-// Express Engine
-import { ngExpressEngine } from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
@@ -40,20 +49,11 @@ app.engine('html', ngExpressEngine({
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
 
-
 /* - Example Express Rest API endpoints -
-app.get('/api/**', (req, res) => { });
+  app.get('/api/**', (req, res) => { });
 */
 
-// Server static files from /browser
-app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
-  maxAge: '1y'
-}));
 
-// All regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render('index', { req });
-});
 
 // ==================
 // GET TREEHOUSE PROFILE DATA
@@ -74,13 +74,20 @@ app.get('/api/getdata', (req, res) => {
 })
 
 
+
+
+
+// Server static files from /browser
+app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
+  maxAge: '1y'
+}));
+
+// ALl regular routes use the Universal engine
+app.get('*', (req, res) => {
+  res.render('index', { req });
+});
+
 // Start up the Node server
 app.listen(PORT, () => {
   console.log(`Node Express server listening on http://localhost:${PORT}`);
 });
-
-
-
-
-
-
